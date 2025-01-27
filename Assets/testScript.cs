4e78a21d734462378642a66888587db6b206342d
alexlehman99 <alexander-lehman@uiowa.edu>
2025-01-27 17:45:30 -0600

    test script moment

---
+++ Assets/testScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
